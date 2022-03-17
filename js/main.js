74280c53cf1a9f1f96ac5ffc4b7711de02a63c6b
knrtaven <52444839+knrtaven@users.noreply.github.com>
2022-03-17 23:24:22 +0800

    added asset index css js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ function scrollHeader(){
     else header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll',scrollHeader)
+
+
+/** mixitup */
+
+let mixerPortfolio = mixitup('.project_container', {
+    selectors: {
+        target: '.project_card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+/* active project link function */
+
+const linkProj = document.querySelectorAll('.project_item')
+
+function activeProject(){
+    linkProj.forEach(i=> i.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkProj.forEach(i=> i.addEventListener('click',activeProject))
