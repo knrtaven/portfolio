cd2d5fc258ee766430b4d7693fbd41a273e4e240
knrtaven <52444839+knrtaven@users.noreply.github.com>
2022-03-17 15:55:33 +0800

    updated html and css

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/** change background header */
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll',scrollHeader)
