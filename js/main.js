5e6cbed99b0f2043d71a645841b6661c0af69bcd
knrtaven <52444839+knrtaven@users.noreply.github.com>
2022-03-18 21:56:10 +0800

    update js index css

--- js/main.js
+++ js/main.js
@@ -31,3 +31,25 @@ function activeProject(){
 }
 
 linkProj.forEach(i=> i.addEventListener('click',activeProject))
+
+
+/**scroll active link */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
