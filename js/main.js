e668a64c1473213be7248daa85f16fb0a3b4d846
knrtaven <52444839+knrtaven@users.noreply.github.com>
2022-03-19 21:04:53 +0800

    updated css

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ function scrollActive(){
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
+              sectionTop = current.offsetTop-58,
               sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
